017abe1896c8937c4a9b88a5bee56b68eb0e76de
Alexandr Merson <alexandrmerson35@gmail.com>
2023-05-05 18:35:44 +0600

    works

--- src/test/java/imit/house/FlatSerializationTest.java
+++ src/test/java/imit/house/FlatSerializationTest.java
@@ -1,2 +1,20 @@
-package imit.house;public class FlatSerializationTest {
+package imit.house;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.testng.annotations.Test;
+
+import static imit.TestingData.FLAT;
+import static org.testng.Assert.assertEquals;
+
+public class FlatSerializationTest {
+    @Test
+    public static void flatSerializationToJsonUsingBin_test() throws JsonProcessingException {
+        final Flat         originalFlat     = FLAT;
+        final ObjectMapper flatMapper       = new ObjectMapper();
+        final String       jsonFlat         = flatMapper.writerWithDefaultPrettyPrinter().writeValueAsString(originalFlat);
+        final Flat         deserializedFlat = flatMapper.readValue(jsonFlat, Flat.class);
+
+        assertEquals(originalFlat, deserializedFlat);
+    }
 }
